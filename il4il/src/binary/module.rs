fdd171fcff967d8dda452d6af516c6cdf3cd7eda
David N <davidcalieln@gmail.com>
2022-08-02 22:33:50 -0700

    Add basic Module methods

--- il4il/src/binary/module.rs
+++ il4il/src/binary/module.rs
@@ -7,3 +7,35 @@ pub struct Module {
     format_version: SupportedFormat,
     sections: Vec<Section>,
 }
+
+impl Module {
+    /// Creates an empty module with the current format version.
+    pub fn new() -> Self {
+        Self { format_version: SupportedFormat::CURRENT, sections: Vec::new() }
+    }
+
+    /// Returns the format version of the module.
+    pub fn format_version(&self) -> SupportedFormat {
+        self.format_version
+    }
+
+    /// Returns a reference module's sections.
+    pub fn sections(&self) -> &Vec<Section> {
+        &self.sections
+    }
+
+    /// Returns a mutable reference to the module's sections.
+    pub fn sections_mut(&mut self) -> &mut Vec<Section> {
+        &mut self.sections
+    }
+
+    /// Appends a section to the end of this module.
+    pub fn append_section<S: Into<Section>>(&mut self, section: S) {
+        self.sections.push(section.into())
+    }
+
+    /// Returns the module's sections.
+    pub fn into_sections(self) -> Vec<Section> {
+        self.sections
+    }
+}
--- il4il/src/binary/section.rs
+++ il4il/src/binary/section.rs
@@ -3,4 +3,7 @@
 /// Represents an IL4IL module section.
 #[derive(Clone, Debug)]
 #[non_exhaustive]
-pub enum Section {}
+pub enum Section {
+    /// The metadata section contains information about the module.
+    Metadata(()),
+}
