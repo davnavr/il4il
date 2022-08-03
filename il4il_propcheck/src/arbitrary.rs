06713720d2aec02be58a6ddc1d09db3686de19cf
David N <davidcalieln@gmail.com>
2022-08-03 13:35:20 -0700

    Add Arb impl for unit

--- il4il_propcheck/src/arbitrary.rs
+++ il4il_propcheck/src/arbitrary.rs
@@ -1,6 +1,22 @@
 //! Contains the [`Arb`] trait.
 
+use crate::generator::{Gen, Rng};
+
 /// Trait used to generate random values.
-pub trait Arb {
-    fn arbitrary<R: rand::Rng + ?Sized>(gen: &mut crate::generator::Gen<'_, R>) -> Self;
+pub trait Arb: std::fmt::Debug {
+    type Shrinker: Iterator<Item = Self>;
+
+    fn arbitrary<R: Rng + ?Sized>(gen: &mut Gen<'_, R>) -> Self;
+
+    fn shrink(&self) -> Self::Shrinker;
+}
+
+impl Arb for () {
+    type Shrinker = std::iter::Empty<()>;
+
+    fn arbitrary<R: Rng + ?Sized>(_: &mut Gen<'_, R>) -> Self {}
+
+    fn shrink(&self) -> Self::Shrinker {
+        std::iter::empty()
+    }
 }
