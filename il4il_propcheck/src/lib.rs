b5f82f9379450a11abdd55129b258b31dd8f3630
David N <davidcalieln@gmail.com>
2022-08-03 14:00:26 -0700

    Add setup module for tests

--- il4il_propcheck/src/lib.rs
+++ il4il_propcheck/src/lib.rs
@@ -5,24 +5,37 @@ pub mod assertion;
 pub mod generator;
 pub mod setup;
 
+fn test_runner<S: setup::Setup, I: arbitrary::Arb, F: Fn(I) -> assertion::Assertion>(test: F) {
+
+}
+
 #[macro_export]
 macro_rules! property {
-    (fn $test_name:ident<$setup_type:ty>($input_type:ty) {
+    (fn $test_name:ident<$setup_type:ty>($input_name:ident: $input_type:ty) {
         $test:expr
     }) => {
         #[test]
-        fn $test_name {
+        fn $test_name() {
             todo!()
         }
     };
 
-    (fn $test_name:ident($input_type:ty) {
+    (fn $test_name:ident($input_name:ident: $input_type:ty) {
         $test:expr
     }) => {
-        property! {{
-            fn $test_name<setup::DefaultSetup>($input_type) {
+        property! {
+            fn $test_name<setup::DefaultSetup>($input_name: $input_type) {
                 $test
             }
-        }}
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    property! {
+        fn left_shift_equals_multiply_by_two(value: u32) {
+            assertion::assertion!(value << 1 == value.overflowing_mul(2).0)
+        }
     }
 }
--- il4il_propcheck/src/setup.rs
+++ il4il_propcheck/src/setup.rs
@@ -5,7 +5,7 @@ use crate::generator::{Gen, Rng};
 pub trait Setup: Default {
     type Rng: Rng + ?Sized;
 
-    fn generator<'a>(&'a mut self) -> Gen<'a, Self::Rng>;
+    fn generator(&mut self) -> Gen<'_, Self::Rng>;
 }
 
 #[derive(Default)]
@@ -14,7 +14,7 @@ pub struct DefaultSetup(rand::rngs::ThreadRng);
 impl Setup for DefaultSetup {
     type Rng = rand::rngs::ThreadRng;
 
-    fn generator<'a>(&'a mut self) -> Gen<'a, Self::Rng> {
+    fn generator(&mut self) -> Gen<'_, Self::Rng> {
         Gen::new(&mut self.0, 65536)
     }
 }
