9aff97ff1dec5c6dd0070ef2f47c5c837cc5500f
David N <davidcalieln@gmail.com>
2022-08-06 23:20:19 -0700

    Fix bug where shrunk test run could not be compiled

--- il4il_propcheck/src/lib.rs
+++ il4il_propcheck/src/lib.rs
@@ -1,87 +1,134 @@
 //! Property testing library.
 
+mod assertion;
+
 pub mod arbitrary;
-pub mod assertion;
 pub mod generator;
 pub mod setup;
+pub mod test;
 
-pub fn run_property_test<S: setup::Setup, I: arbitrary::Arb, F: Fn(I) -> assertion::Assertion>(test: F) {
-    enum Failure {
-        Panic(Box<dyn std::any::Any + Send + 'static>),
-        Message(std::borrow::Cow<'static, str>),
-    }
+pub use arbitrary::Arb;
+pub use assertion::Assertion;
 
-    impl Failure {
-        fn message(&self) -> &str {
-            match self {
-                Self::Panic(_) => "panic occured",
-                Self::Message(message) => message,
-            }
-        }
-    }
-
-    fn run_test<I, F>(value: I, test: &F) -> Result<(), (I, Failure)>
-    where
-        I: arbitrary::Arb,
-        F: Fn(I) -> assertion::Assertion,
-    {
-        let assertion = std::panic::catch_unwind(std::panic::AssertUnwindSafe(|| test(value.clone())));
-
-        match assertion {
-            Ok(assertion::Assertion::Success) => Ok(()),
-            Ok(assertion::Assertion::Failure(message)) => Err((value, Failure::Message(message))),
-            Err(panic_info) => Err((value, Failure::Panic(panic_info))),
-        }
-    }
+pub fn run_property_test<S: setup::Setup, T: test::PropertyTest>(test: T) {
+    let mut arguments_buffer = String::new();
 
     let mut setup = S::default();
-    let mut test_count = setup.test_count();
+    let expected_test_count = setup.test_count();
+    let mut actual_test_count = 0;
     let mut generator = setup.generator();
 
     let failure = loop {
-        if test_count == 0 {
+        if actual_test_count == expected_test_count {
             break Ok(());
         }
 
-        match run_test(I::arbitrary(&mut generator), &test) {
-            Ok(()) => test_count -= 1,
-            Err(error) => break Err(error),
+        match test.clone().test(&mut arguments_buffer, &mut generator) {
+            Ok(test::NonFailure::Success) => actual_test_count += 1,
+            Ok(test::NonFailure::Skipped) => (),
+            Err((shrinker, bad)) => break Err((arguments_buffer.clone(), shrinker, bad)),
         }
     };
 
-    if let Err(error) = failure {
-        eprintln!("test failed with {:?}, {:?}", error.0, error.1.message());
+    if let Err((initial_inputs, shrinker, bad)) = failure {
+        eprintln!("Test failed with ({initial_inputs}), {bad}");
+        let mut failure_count = 1;
 
-        let mut smallest = None;
-        let mut shrunk_count = 0usize;
-        for shrunk in error.0.shrink() {
-            shrunk_count += 1;
-
-            match run_test(shrunk, &test) {
-                Err(f)
-                    if match smallest {
-                        None => true,
-                        Some((ref small, _)) => arbitrary::Comparable::is_smaller_than(small, &f.0),
-                    } =>
-                {
-                    smallest = Some(f)
+        for test in shrinker {
+            match test::ShrunkTest::run(test, &mut arguments_buffer) {
+                Ok(_) => (),
+                Err(bad) => {
+                    eprintln!("> Test failed with ({arguments_buffer}), {bad}");
+                    failure_count += 1;
                 }
-                _ => (),
             }
         }
 
-        let message = if let Some((shrunk, f)) = smallest {
-            eprintln!("shrunk {} times down to {:?}", shrunk_count, shrunk);
-            f
-        } else {
-            error.1
-        };
+        // TODO: Shrink and print the last_error.
+        // TODO: If failure is a panic, then print the whole panic by doing resume_unwind.
+        //let last_error = (initial_inputs, bad);
+        //for test in shrinker { }
 
-        match message {
-            Failure::Message(msg) => panic!("{}", msg),
-            Failure::Panic(panic) => std::panic::resume_unwind(panic),
-        }
+        panic!("Test failed: {actual_test_count} passed, {failure_count} failed")
+    } else if actual_test_count < expected_test_count {
+        panic!(
+            "Unable to generate {expected_test_count} tests, {actual_test_count} passed but {} discarded",
+            expected_test_count - actual_test_count
+        );
     }
+}
+
+// pub fn run_property_test<S: setup::Setup, T: test::Test>(test: T) {
+//     enum Failure {
+//         Panic(Box<dyn std::any::Any + Send + 'static>),
+//         Message(std::borrow::Cow<'static, str>),
+//     }
+
+//     impl Failure {
+//         fn message(&self) -> &str {
+//             match self {
+//                 Self::Panic(_) => "panic occured",
+//                 Self::Message(message) => message,
+//             }
+//         }
+//     }
+
+//     let mut setup = S::default();
+//     let mut test_count = setup.test_count();
+//     let mut arguments_buffer = String::new();
+//     let mut generator = setup.generator();
+
+//     let failure = loop {
+//         if test_count == 0 {
+//             break Ok(());
+//         }
+
+//         match test.run(&mut arguments_buffer, &mut generator) {
+//             Ok(()) => test_count -= 1,
+//             Err(error) => break Err(error),
+//         }
+//     };
+
+//     if let Err(error) = failure {
+//         eprintln!("test failed with {:?}, {:?}", error.0, error.1.message());
+
+//         let mut smallest = None;
+//         let mut shrunk_count = 0usize;
+//         for shrunk in error.0.shrink() {
+//             shrunk_count += 1;
+
+//             match run_test(shrunk, &test) {
+//                 Err(f)
+//                     if match smallest {
+//                         None => true,
+//                         Some((ref small, _)) => arbitrary::Comparable::is_smaller_than(small, &f.0),
+//                     } =>
+//                 {
+//                     smallest = Some(f)
+//                 }
+//                 _ => (),
+//             }
+//         }
+
+//         let message = if let Some((shrunk, f)) = smallest {
+//             eprintln!("shrunk {} times down to {:?}", shrunk_count, shrunk);
+//             f
+//         } else {
+//             error.1
+//         };
+
+//         match message {
+//             Failure::Message(msg) => panic!("{}", msg),
+//             Failure::Panic(panic) => std::panic::resume_unwind(panic),
+//         }
+//     }
+// }
+
+#[macro_export]
+macro_rules! skip {
+    () => {
+        return Option<$crate::Assertion>::None;
+    };
 }
 
 #[macro_export]
@@ -91,7 +138,8 @@ macro_rules! property {
     }) => {
         #[test]
         fn $test_name() {
-            $crate::run_property_test::<$setup_type, $input_type, _>(|$input_name| $test);
+            //$crate::run_property_test::<$setup_type, $input_type, _>(|$input_name| $test);
+            todo!()
         }
     };
 
@@ -106,9 +154,27 @@ macro_rules! property {
     };
 }
 
+#[macro_export]
+macro_rules! assertion {
+    ($e:expr) => {
+        Some(if $e {
+            $crate::Assertion::Success
+        } else {
+            $crate::Assertion::Failure(concat!("assertion failed: ", stringify!($e)).into())
+        })
+    };
+}
+
+#[macro_export]
+macro_rules! assertion_eq {
+    ($expected:expr, $actual:expr) => {
+        Some($crate::Assertion::are_equal($expected, $actual))
+    };
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::assertion;
+    use crate::*;
 
     property! {
         fn left_shift_equals_multiply_by_two(value: u32) {
