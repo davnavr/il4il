1970b7ffa9fc99f16982f8636f7131096c94748a
David N <davidcalieln@gmail.com>
2022-08-15 15:08:46 -0700

    Add NameMetadata.cs

---
+++ il4il_sharp/src/Il4ilSharp/ModuleMetadata.cs
@@ -0,0 +1,30 @@
+﻿namespace Il4ilSharp;
+
+using System;
+using Il4ilSharp.Interop;
+using Il4ilSharp.Interop.Native;
+
+/// <summary>Base class for representations of IL4IL module metadata.</summary>
+public abstract class ModuleMetadata {
+    /// <summary>Gets a handle for the underlying module metadata.</summary>
+    public MetadataHandle Handle { get; }
+
+    /// <summary>Gets a value indicating the kind of metadata.</summary>
+    public MetadataKind Kind { get; }
+
+    private protected ModuleMetadata(MetadataHandle handle, MetadataKind kind) {
+        Handle = handle;
+        Kind = kind;
+    }
+
+    internal static ModuleMetadata Create(MetadataHandle handle) {
+        ArgumentNullException.ThrowIfNull(handle);
+        MetadataKind kind = handle.GetKind();
+        switch (kind) {
+            case MetadataKind.Name:
+                return new NameMetadata(handle);
+            default:
+                throw new InvalidOperationException(kind + " is not a valid metadata kind");
+        }
+    }
+}
--- il4il_sharp/src/Il4ilSharp/ValidModule.cs
+++ il4il_sharp/src/Il4ilSharp/ValidModule.cs
@@ -1,29 +1,37 @@
-namespace Il4ilSharp;
+﻿namespace Il4ilSharp;
 
 using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using Il4ilSharp.Interop;
 
-/// <summary>
-/// Represents an IL4IL module which has been validated.
-/// </summary>
+/// <summary>Represents an IL4IL module which has been validated.</summary>
 /// <remarks>This class provides various methods to examine the contents of IL4IL modules.</remarks>
 public sealed class ValidModule {
     /// <summary>Gets the underlying handle to the browser used to read the content of the IL4IL module.</summary>
     public BrowserHandle Browser { get; } // TODO: Maybe have an interface for this so inheritdoc can be used?
 
-    internal ValidModule(BrowserHandle browser) {
-        ArgumentNullException.ThrowIfNull(nameof(browser));
+    /// <summary>Gets the contents of the module's metadata section.</summary>
+    public IReadOnlyList<ModuleMetadata> Metadata { get; }
 
-        try {
-            unsafe {
-                browser.Lock(); // Prevent handle from being disposed early
-            }
-
-            Browser = browser;
-        } finally {
-            browser.Unlock();
+    private static IReadOnlyList<ModuleMetadata> InitializeMetadata(BrowserHandle browser) {
+        var handles = browser.GetMetadata();
+        var metadata = new ModuleMetadata[handles.Length];
+        for (int i = 0; i < metadata.Length; i++) {
+            metadata[i] = ModuleMetadata.Create(handles[i]);
         }
+
+        return new ReadOnlyCollection<ModuleMetadata>(metadata);
     }
 
+    /// <summary>Initializes a <see cref="ValidModule"/> with the specified <see cref="BrowserHandle"/>.</summary>
+    /// <exception cref="ArgumentNullException">Thrown if the <paramref name="browser"/> is <see langword="null"/>.</exception>
+    public ValidModule(BrowserHandle browser) {
+        ArgumentNullException.ThrowIfNull(nameof(browser));
+        Browser = browser;
+        Metadata = InitializeMetadata(browser);
+    }
+
+    /// <summary>Initializes a <see cref="ValidModule"/> from the contents of a <see cref="Interop.ModuleHandle"/>.</summary>
     public ValidModule(Interop.ModuleHandle module) : this(module.ValidateAndDispose()) { }
 }
