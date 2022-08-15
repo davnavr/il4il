9cad7bcca368c967caf615e563828582ddca157a
David N <davidcalieln@gmail.com>
2022-08-15 16:10:06 -0700

    Add test to check empty module is parsed correctly

--- il4il/src/binary/mod.rs
+++ il4il/src/binary/mod.rs
@@ -12,3 +12,18 @@ pub mod writer;
 
 /// The magic number that is the start of all IL4IL module files.
 pub const MAGIC: &[u8; 6] = b"IL4IL\0";
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parsed_empty_module_is_empty() {
+        let builder = Module::new();
+        let mut buffer = Vec::new();
+        builder.write_to(&mut buffer).unwrap();
+
+        let parsed = Module::read_from(buffer.as_slice()).unwrap();
+        assert!(parsed.into_sections().is_empty());
+    }
+}
