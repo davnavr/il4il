10cc14cc659283835054a6dedb874e0ba775c7d0
David N <davidcalieln@gmail.com>
2022-08-18 13:00:51 -0700

    Add function::Signature struct

--- il4il/src/function.rs
+++ il4il/src/function.rs
@@ -4,14 +4,41 @@
 
 use crate::type_system;
 
-/// Function signatures specify the parameter types and return types of functions.
+/// Function signatures specify the parameter types and result types of functions.
 #[derive(Clone, Debug, Default, Eq, PartialEq)]
 pub struct Signature {
     types: Box<[type_system::Reference]>,
-    return_type_count: usize,
+    result_type_count: usize,
 }
 
 impl Signature {
+    /// Creates a function signature with the specified result types followed by the parameter types.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the specified number of result types exceeds the total number of types.
+    pub fn from_types<T: Into<Box<[type_system::Reference]>>>(types: T, result_type_count: usize) -> Self {
+        let signature_types = types.into();
+        assert!(result_type_count <= signature_types.len());
+        Self { types: signature_types, result_type_count }
+    }
+
+    pub fn result_type_count(&self) -> usize {
+        self.result_type_count
+    }
+
+    pub fn parameter_type_count(&self) -> usize {
+        self.types.len() - self.result_type_count
+    }
+
+    pub fn result_types(&self) -> &[type_system::Reference] {
+        &self.types[0..self.result_type_count]
+    }
+
+    pub fn parameter_types(&self) -> &[type_system::Reference] {
+        &self.types[self.result_type_count..]
+    }
+
     pub fn into_types(self) -> Box<[type_system::Reference]> {
         self.types
     }
