cb5014f7dc07ce9bc8cef87b77cb2b7035437e16
David N <davidcalieln@gmail.com>
2022-08-19 17:11:03 -0700

    Add ModuleContents conversions

--- il4il/src/validation/contents.rs
+++ il4il/src/validation/contents.rs
@@ -1,6 +1,7 @@
 //! Contains types that model the contents of a valid IL4IL module.
 
 use crate::module::section::{self, Section};
+use crate::module::Module;
 use crate::type_system;
 
 /// Represents the contents of a SAILAR module.
@@ -34,11 +35,54 @@ impl<'data> ModuleContents<'data> {
         Ok(contents)
     }
 
+    #[must_use]
     pub fn from_sections<S: IntoIterator<Item = Section<'data>>>(sections: S) -> Self {
         Self::from_sections_fallible(sections.into_iter().map(Result::<_, std::convert::Infallible>::Ok)).unwrap()
     }
 
-    pub fn from_module(module: crate::module::Module<'data>) -> Self {
+    #[must_use]
+    pub fn from_module(module: Module<'data>) -> Self {
         Self::from_sections(module.into_sections())
+    }
+
+    /// Converts the module contents into a sequence of sections.
+    #[must_use]
+    pub fn into_sections(self) -> Box<[Section<'data>]> {
+        let mut sections = {
+            let mut capacity = 0;
+
+            if !self.metadata.is_empty() {
+                capacity += 1;
+            }
+
+            if !self.types.is_empty() {
+                capacity += 1;
+            }
+
+            Vec::with_capacity(capacity)
+        };
+
+        sections.push(Section::Metadata(self.metadata));
+        sections.push(Section::Type(self.types));
+        sections.into_boxed_slice()
+    }
+
+    #[must_use]
+    pub fn into_module(self) -> Module<'data> {
+        let mut module = Module::new();
+        *module.sections_mut() = self.into_sections().into_vec();
+        module
+    }
+}
+
+impl<'data> From<Module<'data>> for ModuleContents<'data> {
+    fn from(module: Module<'data>) -> Self {
+        Self::from_module(module)
+    }
+}
+
+impl<'data> From<ModuleContents<'data>> for Module<'data> {
+    fn from(contents: ModuleContents<'data>) -> Self {
+        contents.into_module()
     }
 }
