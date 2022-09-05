be08a9c97fcf24eecac849b6b1a67941769f451d
David N <davidcalieln@gmail.com>
2022-09-04 19:31:15 -0700

    Add loader types for type references

--- il4il_loader/src/types.rs
+++ il4il_loader/src/types.rs
@@ -49,3 +49,54 @@ impl<'env> PartialEq for &'env Type<'env> {
         std::ptr::eq(self.module as *const _, other.module as *const _) && self.kind() == other.kind()
     }
 }
+
+// TODO: Rename to ReferenceKind, add a struct Reference
+pub enum ReferenceKind<'env> {
+    Inlined(Type<'env>),
+    Indexed(&'env Type<'env>),
+}
+
+impl<'env> ReferenceKind<'env> {
+    pub fn as_type(&self) -> &Type<'env> {
+        match self {
+            Self::Inlined(inlined) => inlined,
+            Self::Indexed(indexed) => indexed,
+        }
+    }
+}
+
+type ReferenceInner<'env> = lazy_init::LazyTransform<(&'env Module<'env>, type_system::Reference), ReferenceKind<'env>>;
+
+pub struct Reference<'env>(ReferenceInner<'env>);
+
+impl<'env> Reference<'env> {
+    pub(crate) fn new(module: &'env Module<'env>, ty: type_system::Reference) -> Self {
+        Self(ReferenceInner::new((module, ty)))
+    }
+
+    pub fn kind(&self) -> &ReferenceKind<'env> {
+        self.0.get_or_create(|(module, ty)| match ty {
+            type_system::Reference::Inline(inlined) => ReferenceKind::Inlined(Type::new(module, inlined)),
+            type_system::Reference::Index(index) => ReferenceKind::Indexed(&module.types()[usize::from(index)]),
+        })
+    }
+
+    pub fn as_type(&self) -> &Type<'env> {
+        self.kind().as_type()
+    }
+}
+
+type ReferenceListInner<'env> = lazy_init::LazyTransform<(&'env Module<'env>, Box<[type_system::Reference]>), Box<[Reference<'env>]>>;
+
+pub struct ReferenceList<'env>(ReferenceListInner<'env>);
+
+impl<'env> ReferenceList<'env> {
+    pub(crate) fn new(module: &'env Module<'env>, types: Box<[type_system::Reference]>) -> Self {
+        Self(ReferenceListInner::new((module, types)))
+    }
+
+    pub fn types(&self) -> &[Reference<'env>] {
+        self.0
+            .get_or_create(|(module, references)| references.into_vec().into_iter().map(|r| Reference::new(module, r)).collect())
+    }
+}
