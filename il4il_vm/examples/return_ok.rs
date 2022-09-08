18b5005e358b01e103d8655d4cc8e87bd33ca871
David N <davidcalieln@gmail.com>
2022-09-08 15:07:56 -0700

    Update return_ok sample to use None resolver

--- il4il_vm/examples/return_ok.rs
+++ il4il_vm/examples/return_ok.rs
@@ -3,7 +3,7 @@ use il4il_vm::runtime;
 fn main() {
     let program = il4il_samples::return_int("Ok", 1);
     let runtime = runtime::Runtime::new();
-    let module = runtime.load_module(program);
+    let module = runtime.load_module(program, None);
     let mut interpreter = module.interpret_entry_point(Default::default()).expect("entry point should exist");
     let return_values = loop {
         if let Some(values) = interpreter.step().unwrap() {
@@ -12,7 +12,7 @@ fn main() {
     };
 
     assert_eq!(
-        il4il_vm::interpreter::Value::into_u32(return_values[0].clone(), runtime.configuration().endianness),
+        il4il_vm::interpreter::value::Value::into_u32(return_values[0].clone(), runtime.configuration().endianness),
         1u32
     );
 }
