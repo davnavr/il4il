baad1773b7eb18d9f3a156da142ae43b0558c722
David N <davidcalieln@gmail.com>
2022-09-09 22:55:31 -0700

    Update lazy debug view

--- il4il_loader/src/debug.rs
+++ il4il_loader/src/debug.rs
@@ -7,6 +7,10 @@ pub struct LazyDebug<'a, T, U>(pub &'a lazy_init::LazyTransform<T, U>);
 
 impl<T, U: Debug> Debug for LazyDebug<'_, T, U> {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        Debug::fmt(&self.0.get(), f)
+        if let Some(value) = &self.0.get() {
+            Debug::fmt(value, f)
+        } else {
+            f.write_str("<uninitialized>")
+        }
     }
 }
