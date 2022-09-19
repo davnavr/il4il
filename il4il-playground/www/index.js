91344451ed3d0f2c55ebd83bfa8efda978410986
David N <davidcalieln@gmail.com>
2022-09-19 12:47:01 -0700

    Add JS code for playground tabs

--- il4il-playground/www/index.js
+++ il4il-playground/www/index.js
@@ -1,3 +1,62 @@
 import * as playground from "il4il-playground"
 
-console.log("Hello!", playground.do_something(5));
+/**
+ * @type {{ name: string; content: (tab: HTMLElement) => void }[]}
+ */
+const TABS = [
+    {
+        name: "IL4IL",
+        content: (tab) => {
+
+        },
+    }
+];
+
+function initialize_tabs() {
+    const tabs_nav = window.document.getElementById("tabs-nav");
+    const tabs_div = window.document.getElementById("tabs-div");
+
+    /**
+     * @type {HTMLButtonElement}
+     */
+    let first_tab = null;
+
+    /**
+     * @type {{ tab: HTMLElement, button: HTMLButtonElement } | null}
+     */
+    let active_tab = null;
+
+    TABS.forEach(tab => {
+        const button = window.document.createElement("button");
+        const section = window.document.createElement("section");
+
+        if (first_tab === null) {
+            first_tab = button;
+        }
+
+        tabs_nav.appendChild(button);
+        tabs_div.appendChild(section);
+
+        button.classList.add("tab-button");
+        button.innerText = tab.name;
+
+        section.classList.add("hidden");
+
+        button.addEventListener("click", () => {
+            if (active_tab !== null) {
+                active_tab.tab.classList.add("hidden");
+                active_tab.button.disabled = false;
+            }
+
+            section.classList.remove("hidden");
+            button.disabled = true;
+            active_tab = { tab: section, button };
+        });
+
+        tab.content(section);
+    });
+
+    first_tab.click();
+}
+
+initialize_tabs();
